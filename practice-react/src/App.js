d99f598eb0e2041f45f41501c4fd262c8bc1d545
andershup <anders242@gmail.com>
2022-06-02 09:19:04 +0000

    logo imported and add to navigation. Sign-up-sign-in page added with sign-in and sign-up moved to components

--- practice-react/src/App.js
+++ practice-react/src/App.js
@@ -2,7 +2,8 @@
 import { Routes, Route } from 'react-router-dom'
 import NavigationBar from './routes/navigation-bar/navigation-bar.component'
 import Directory from "./routes/directory/directory.component";
-import SignIn from "./routes/sign-in/sign-in.component"
+import SignIn from "./components/sign-in/sign-in.component"
+import SignUp from "/routes/sign-up/sign-up.component"
 
 
 const Shop = () => {
@@ -20,6 +21,7 @@ const App = () => {
             <Route path='/' element={<NavigationBar/> }>
               <Route index element={<Directory />} />\
               <Route path='sign-in' element={<SignIn/>} />
+              <Route path='sign-up' element={<SignUp/>} />
               <Route path='shop' element={<Shop/>} />
             </Route>
           </Routes>
--- practice-react/src/routes/navigation-bar/navigation-bar.component.jsx
+++ practice-react/src/routes/navigation-bar/navigation-bar.component.jsx
@@ -1,11 +1,16 @@
 import './navigation-bar.styles.scss'
-import { Outlet} from 'react-router-dom'
+import { Outlet, Link} from 'react-router-dom'
+import { ReactComponent as Logo } from'../../assets/disc.svg'
 const NavigationBar = () => {
 
     return (
         <>
         <div className="navigation">
-        test text 
+        <Link className="logo" to="/">
+        <Logo className="actual-logo"/>
+            </Link>
+        test text text text 
+        <Link className='nav-link' to="/sign-in">Sign In</Link>
         </div>
         <Outlet/>
         </>
